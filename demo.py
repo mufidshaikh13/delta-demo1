5e531803e4f3a7fdfc348d1c3c617292d4e1a7a7
mufid shaikh <mufidshaikh04@gmail.com>
2025-02-26 18:25:26 +0530

    prime number

--- demo.py
+++ demo.py
@@ -1,1 +1,15 @@
-print("hello")
+def prime(n):
+    is_prime = False
+    for i in range(2,n):
+        if n % i == 0:
+            return True
+    
+    return False
+
+
+if prime(10):
+    print("prime")
+else:
+    print("composite")
+
+    
